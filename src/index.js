992323c329b4e4c6ba7de4cf4af59710d0869b0d
Kyle Schafer <kschay@protonmail.com>
2019-08-20 22:45:18 -0500

    Closes #3 - Fetched user's current location

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
-  return <SeasonDisplay />
+  // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
+  return <div>Latitude: </div>
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
