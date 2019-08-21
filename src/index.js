7db257bf8f4815d49303f12716890d2fb1d267fe
Kyle Schafer <kschay@protonmail.com>
2019-08-20 23:17:56 -0500

    Closes #5 - Rendered user's location with state

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
+//must extend React.Component
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    //must be called
+    super(props);
+
+    //initialize state with object
+    this.state = { latitude: null };
+
+    //get user location
     // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      //callbacks not called during construction
+
+      //on success
+      (position) => {
+        //setState must be called to update state after initialization
+        this.setState({ latitude: position.coords.latitude });
+      },
+      //on failure
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>
-
+  //render must be defined in React class
+  render() {
+    return <div>Latitude: {this.state.latitude}</div>
   }
 }
 
